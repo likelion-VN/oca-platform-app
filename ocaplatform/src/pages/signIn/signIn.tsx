c1c702bde02ca3d732e344e24943ed121b67a45e
Hoang-Tien <thtien0110@likelion.net>
2024-10-17 12:40:59 +0700

    set up login in local

--- ocaplatform/src/pages/signIn/signIn.tsx
+++ ocaplatform/src/pages/signIn/signIn.tsx
@@ -29,6 +29,7 @@ const LoginPage = () => {
   };
 
   const loginByGoogle = () => {
+    // *: For production
     const width = 500;
     const height = 600;
     const left = window.screenX + window.outerWidth / 2 - width / 2;
@@ -38,6 +39,17 @@ const LoginPage = () => {
       "Login by Google",
       `width=${width},height=${height},top=${top},left=${left}`
     );
+
+    // *: For developer
+    // auth.setIsLogin(true);
+    // const idToken = 'eyJhbGciOiJSUzI1NiIsImtpZCI6ImE1MGY2ZTcwZWY0YjU0OGE1ZmQ5MTQyZWVjZDFmYjhmNTRkY2U5ZWUiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJhenAiOiI0MjIwNjY1MjU4OTEtZW02MnVubjhranNrNXVpMGM1Zzh1MHNxNWxscDMxY3MuYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJhdWQiOiI0MjIwNjY1MjU4OTEtZW02MnVubjhranNrNXVpMGM1Zzh1MHNxNWxscDMxY3MuYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJzdWIiOiIxMTUxNzUyODgzNTMwMjA3NTQ2MjMiLCJoZCI6Imxpa2VsaW9uLm5ldCIsImVtYWlsIjoidGh0aWVuMDExMEBsaWtlbGlvbi5uZXQiLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiYXRfaGFzaCI6ImNCbW1jMFU1TmY4RTl1NnlwLThteUEiLCJub25jZSI6IkNjbTJyS0VwRVZvWVU5b2RLblhoNHJ4d200OW1UTzFYbW1kZjlCNnM0U1UiLCJuYW1lIjoidGh0aWVuMDExMCB0aHRpZW4wMTEwIiwicGljdHVyZSI6Imh0dHBzOi8vbGgzLmdvb2dsZXVzZXJjb250ZW50LmNvbS9hL0FDZzhvY0xsX2I2aHRsbVBKSnl2SEZUQU1xQVRSZDRNdnJXcGVjTGNBTWlEa3c4TnFhdlFfQT1zOTYtYyIsImdpdmVuX25hbWUiOiJ0aHRpZW4wMTEwIiwiZmFtaWx5X25hbWUiOiJ0aHRpZW4wMTEwIiwiaWF0IjoxNzI5MTQwODMwLCJleHAiOjE3MjkxNDQ0MzB9.cb57UiUT0XMKnWGQQCj6RDpEppypV0-YhijX_aEtP8S6TbB0QhqJt2kQR-qrku8HSm4d20qYvtT8gWeDtcbh3STDpzDTj_mp_Ht9OwboPDzsBzleRZ7PwDibQjXedZdhqPpHEtHhjHR0CjAXYvAHsK16FqV_7JnGgoB87gMSYV5ZmSqCoCkq1QhGNphOm1jojEDpF5qehOgJr3-5usZxmdKufr3a3X8e1UItAMD8v4NeZZAsYO-FYI0cOl_oZGXOhA7wk28q02ZqbusGnmTuKaQHSNf2tXdSLRM_y5rhelT5nsFsxjk1k7b9yfvFNBPy0DvHxrCKUcvk5fTiQ_Cyig';
+    // const email = 'thtien0110@likelion.net';
+    // document.cookie = `user_token=${idToken}; path=/; secure`;
+    // document.cookie = `user_email=${encodeURIComponent(email)}; path=/; secure`;
+    // // Set accout type
+    // auth.setCandidateUser(true);
+    // // auth.setCompanyUser(true);
+    // navigate("/dash-board");
   };
 
   useEffect(() => {
