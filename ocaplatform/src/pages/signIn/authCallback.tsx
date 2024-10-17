c99088a113c80275436f867ff9c58e5dc6868a4f
Hoang-Tien <thtien0110@likelion.net>
2024-10-17 10:18:06 +0700

    rebuild

--- ocaplatform/src/pages/signIn/authCallback.tsx
+++ ocaplatform/src/pages/signIn/authCallback.tsx
@@ -2,21 +2,23 @@ import { useEffect } from "react";
 
 const AuthCallback: React.FC = () => {
   useEffect(() => {
-    const params = new URLSearchParams(window.location.hash.slice(1));
+    const parseUrlParams = () => {
+      const params = new URLSearchParams(window.location.hash.slice(1));
+      const allParams: { [key: string]: string | null } = {};
 
-    const allParams: { [key: string]: string | null } = {};
-    
-    params.forEach((value, key) => {
-      allParams[key] = decodeURIComponent(value);
-    });
+      params.forEach((value, key) => {
+        allParams[key] = decodeURIComponent(value);
+      });
 
-    console.log('test', allParams)
+      return allParams;
+    };
+
+    const allParams = parseUrlParams();
+
+    console.log("test", allParams);
 
     if (allParams.access_token) {
-      window.opener?.postMessage(
-        { params: allParams },
-        window.location.origin
-      );
+      window.opener?.postMessage({ params: allParams }, window.location.origin);
     }
     // window.close();
   }, []);
@@ -24,4 +26,4 @@ const AuthCallback: React.FC = () => {
   return <div>Processing authentication...</div>;
 };
 
-export default AuthCallback; 
+export default AuthCallback;
